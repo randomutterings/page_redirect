88d191df7ef3ae1a1a4bbc4d35e570add08bf377
Chris <chris@Chris-Macbook-Pro.local>
2008-04-17 12:50:47 -0400

    corrected bug with status codes not being sent properly

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,5 +1,5 @@
 class Redirect < Page
   def process(request, response)
-    response.redirect(part(:body).content, part(:http_status).content)
+    response.redirect(part(:body).content, :status => part(:http_status).content)
   end
 end
--- test/unit/redirect_test.rb
+++ test/unit/redirect_test.rb
@@ -3,8 +3,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class RedirectTest < Test::Unit::TestCase
   
   def test_redirected
-    #redirect = Redirect.process("http://www.randomutterings.com", "302")
-    #assert_redirected_to("http://www.randomutterings.com", "302")
+    #redirect = Redirect.process("http://www.randomutterings.com", "302 Found")
+    #assert_redirected_to("http://www.randomutterings.com", "302 Found")
   end
   
 end
